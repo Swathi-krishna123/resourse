72735c3cd245d95a0ed5c8a23fb763834b6ef6c1
swathi Krishna TS <swathikrishna2592@gmail.com>
2024-11-26 23:44:07 +0530

    update

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -52,12 +52,13 @@ class Homepage extends StatelessWidget {
                       hintText: 'Search',
                       hintStyle: TextStyle(
                         fontWeight: FontWeight.w300,
-                        fontSize: 20,
+                        fontSize: 15,
                         color: Appcolors.hintColor,
                       ),
                       prefixIcon: const Icon(Icons.search),
                       suffixIcon: Container(
-                        margin: const EdgeInsets.only(right: 6,top: 6,bottom: 6),
+                        margin:
+                            const EdgeInsets.only(right: 6, top: 6, bottom: 6),
                         width: 44,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(6),
@@ -84,6 +85,55 @@ class Homepage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 10),
+                Obx(
+                  () => controller.selectedItems.isEmpty
+                      ? SizedBox.shrink()
+                      : Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              margin: const EdgeInsets.only(
+                                  right: 10), // Add margin between items
+                              decoration: BoxDecoration(
+                                color: Appcolors.themeColor.withOpacity(0.14),
+                                borderRadius: BorderRadius.circular(4),
+                                border: Border.all(color: Appcolors.themeColor),
+                              ),
+                              child: Text(
+                                'Refresh',
+                                style: TextStyle(
+                                  color: Appcolors.blackColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              margin: const EdgeInsets.only(
+                                  right: 10), // Add margin between items
+                              decoration: BoxDecoration(
+                                color: Appcolors.themeColor.withOpacity(0.14),
+                                borderRadius: BorderRadius.circular(4),
+                                border: Border.all(color: Appcolors.themeColor),
+                              ),
+                              child: Text(
+                                'Share Resources',
+                                style: TextStyle(
+                                  color: Appcolors.blackColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
 
                 // Selected Items Display
                 // Selected Items Display
@@ -94,49 +144,46 @@ class Homepage extends StatelessWidget {
                   }
                   return SingleChildScrollView(
                     scrollDirection: Axis.horizontal, // Scroll horizontally
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        children: controller.selectedItems.map((item) {
-                          final name = item['nm'] ?? 'Unknown Name';
-                          return Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 5),
-                            margin: const EdgeInsets.only(
-                                right: 10), // Add margin between items
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              border: Border.all(color: Appcolors.blackColor),
-                            ),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  name,
-                                  style: TextStyle(
-                                    color: Appcolors.blackColor,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                  ),
+                    child: Row(
+                      children: controller.selectedItems.map((item) {
+                        final name = item['nm'] ?? 'Unknown Name';
+                        return Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 5),
+                          margin: const EdgeInsets.only(
+                              right: 10), // Add margin between items
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(color: Appcolors.blackColor),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                name,
+                                style: TextStyle(
+                                  color: Appcolors.blackColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
                                 ),
-                                const SizedBox(width: 5),
-                                GestureDetector(
-                                  onTap: () {
-                                    // Remove the item when delete icon is clicked
-                                    controller.selectedItems.remove(item);
-                                    controller.selectedItems.refresh();
-                                  },
-                                  child: const Icon(
-                                    Icons.close,
-                                    color: Colors.red,
-                                    size: 18,
-                                  ),
+                              ),
+                              const SizedBox(width: 5),
+                              GestureDetector(
+                                onTap: () {
+                                  // Remove the item when delete icon is clicked
+                                  controller.selectedItems.remove(item);
+                                  controller.selectedItems.refresh();
+                                },
+                                child: const Icon(
+                                  Icons.close,
+                                  color: Colors.red,
+                                  size: 18,
                                 ),
-                              ],
-                            ),
-                          );
-                        }).toList(),
-                      ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
                     ),
                   );
                 }),
